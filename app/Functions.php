c2aaf7cbba4e0604c2492b62f8e46c68cb726fb8
Jorge Sepulveda <jorluiseptor@gmail.com>
2025-10-22 16:44:54 -0400

    Add PHPUnit configuration and a sample unit test
    - Created phpunit.xml for PHPUnit configuration, defining test suites and environment variables for testing.
    - Added ExampleTest.php in the Unit tests directory with a basic test case to verify that true is true.

---
+++ app/Functions.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types= 1);
+
+namespace App;
+
+function add(int $a, int $b): int
+{
+    return $a + $b;
+}
+function subtract(int $a, int $b): int
+{
+    return $a - $b;
+}
